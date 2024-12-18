8c27952c14a690d7b03ed8357dbbfe52b5f12c0f
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2024-12-19 00:35:02 +0530

    Learing variables

---
+++ variables.js
@@ -0,0 +1,16 @@
+const number = 123456 
+/*
+Once const is assigened it cant be changed it will give
+:: Assignment to constant variable at Object.<anonymous>
+*/
+console.log("const number => " + number);
+
+let email = "Abhishek.com" 
+/* 
+ We have to use let cause it has the scope defination foor block or funcational scope
+  where as the var has no ide of scope defination.
+*/
+var name = "Abhishek"
+city = "Bhopal" 
+// If we want we can change these values 
+console.table([email , name , city])
