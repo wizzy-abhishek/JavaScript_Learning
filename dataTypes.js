29ae4e7a76a592cfde6c76ce4fb6e9d1067cd216
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2024-12-19 21:44:38 +0530

    Datatypes

---
+++ dataTypes.js
@@ -0,0 +1,36 @@
+"use strict"; // Treat all JS code as newer version
+
+//alert(3+3) // We are using nodeJs , not browser
+
+console.log(3+3); console.log("Abhishek") //Not readable code -- Code readability should be high
+
+// tc39 for js documentation and standarization
+// and mdn for beginners
+
+let name = "Abhishek"; //String
+let age = 21 ; //int
+let isLoggedIn = false // boolean
+
+
+/* Premitive */
+//number => 2 to power 53 max value 
+//bigInt
+//String => " --- " , '---'
+//boolean 
+//null -> standalone value (representation of empty value)
+//underfined -> the value is not assigned 
+//symbol 
+
+/* Not premitive */
+//object 
+
+
+console.log(typeof age);
+//typeof tells the type of data 
+
+
+// null is a type of Object
+console.log(typeof null);
+
+//where as undefined is type undefined
+console.log(typeof undefined)
