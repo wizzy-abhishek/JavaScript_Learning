67ec3c554040705d4064844f89703c6b94ed3575
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2024-12-28 01:16:23 +0530

    String started

---
+++ 09_string.js
@@ -0,0 +1,9 @@
+let firstName = "Abhishek"
+let lastName = "Anand"
+
+console.log(firstName + " " + lastName) //This is very not good way to write
+
+// So use back ticks it give string interpoletion and it use place holder
+
+console.log(`Hello my name is ${firstName} ${lastName}`)
+
