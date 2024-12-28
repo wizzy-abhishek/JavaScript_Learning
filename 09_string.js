1df344aef30ae1138dfa01a857e702a87977a800
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2024-12-29 01:57:55 +0530

    String

--- 09_string.js
+++ 09_string.js
@@ -7,3 +7,43 @@ console.log(firstName + " " + lastName) //This is very not good way to write
 
 console.log(`Hello my name is ${firstName} ${lastName}`)
 
+// Another way to declare string 
+
+/*
+const myName = new String("Abhishek");
+
+console.log(myName === firstName);
+
+console.log(myName.__proto__);
+console.log(myName.length)
+console.log(myName.toUpperCase());
+console.log(myName[0]);
+console.log(myName.charAt(3))
+
+*/
+
+const myGf = new String("..GARGI..");
+
+console.log(myGf.indexOf("G"));
+
+// const newName = myGf.substring(2,7);
+
+// console.log(newName);
+
+const useOfSlice = myGf.slice(-4 , 1);
+console.log(useOfSlice);
+
+
+const url = "https://abhishek.com/abhishek%20anand";
+
+const modifiedUrl = url.replace("%20" ,"-");
+
+console.log(modifiedUrl);
+
+const newFeature = "abhishek-anand-84";
+
+const array = newFeature.split("-");
+
+console.log(array);
+
+
