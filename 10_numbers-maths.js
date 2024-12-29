997314f1713435027ba0bb53530139e776772dbd
wizzy-abhishek <workspace.abhishek.08@gmail.com>
2024-12-30 02:09:34 +0530

    Number and maths

---
+++ 10_numbers-maths.js
@@ -0,0 +1,12 @@
+const score = 400 ;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+
+console.log(balance.toFixed(3)); //upto 3 decimal values
+
+console.log(balance.toFixed(3).toString().length);
+
+const otherNumber = 1234.9234 ;
+console.log(otherNumber.toPrecision(4)); //Rounds of to the value passed as parameter rest is exponential 
